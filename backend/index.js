b6151602a4955dcaf2c74b526f4f69c0af84244f
aninda8680 <anindadebta8680@gmail.com>
2026-04-10 17:35:54 +0530

    chore: update dependencies and regenerate lockfile

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,16 @@ const usersDatabase = [
 // Simple College Scraping Function Using Axios and CookieJar
 async function scrapeCollegeAttendance(user, type) {
   const jar = new CookieJar();
-  const client = wrapper(axios.create({ jar, withCredentials: true }));
+  const client = wrapper(axios.create({ 
+    jar, 
+    withCredentials: true,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Connection': 'keep-alive'
+    }
+  }));
 
   try {
     // 1. Fetch Login Page to get CSRF token
@@ -73,7 +82,10 @@ async function scrapeCollegeAttendance(user, type) {
       password: user.password,
       login: "login"
     }, {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      headers: { 
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Referer": baseUrl + "login"
+      }
     });
 
     // 3. Fetch Dashbaord or Attendance page based on request
